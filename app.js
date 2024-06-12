cad5a224995b41d22d836706b1d5af472622798b
Stinamariya <stinamariya2006@gmail.com>
2024-06-13 03:23:53 +0530

    search started

--- app.js
+++ app.js
@@ -26,6 +26,20 @@ app.get("/view",(req,res)=>{
         }
     )
 })
+app.post("/search",(req,res)=>{
+    let input=req.body
+    eventmodel.find(input).then(
+        (data)=>{
+            res.json(data)
+            
+        }
+    ).catch(
+        (error)=>{
+            res.json(error)
+        }
+    )
+        
+})
 
 
 app.listen(8081,()=>{
